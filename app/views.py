4ee7e8ac7e8dfb18c15f516f3eaa8c3cb489f7e9
Rodgersouko <gabrielaouko@gmail.com>
2020-10-17 10:39:24 +0300

    declare functins and if statments using blocks inside html structure

--- app/views.py
+++ app/views.py
@@ -1,13 +1,25 @@
 from flask import render_template
 from app import app
 
+
+
 # Views
-@app.route('/news/<int:news_id>')
-def news(news_id):
+@app.route('/')
+def index():
 
     '''
     View root page function that returns the index page and its data
     '''
 
-    # news = 'Corona Virus'
-    return render_template('news.html',id = news_id)
+    title = 'Home - Welcome to The best News Update Website Online'
+    return render_template('index.html', title = title)
+
+
+@app.route('/news/<news_id>')
+def news(news_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    title = f'You are viewing {news_id}'
+    return render_template('news.html',title = title)
