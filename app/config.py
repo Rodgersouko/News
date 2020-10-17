244883403dc7529c1dd5370d40b43379180119a6
Rodgersouko <gabrielaouko@gmail.com>
2020-10-17 09:52:45 +0300

    set configurations production  & development

--- app/config.py
+++ app/config.py
@@ -1,3 +1,27 @@
-#excluding virtual environment and compiled python files
-virtual/
-*.pyc
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
--- app/views.py
+++ app/views.py
@@ -2,12 +2,12 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
-def index():
+@app.route('/news/<int:news_id>')
+def news(news_id):
 
     '''
     View root page function that returns the index page and its data
     '''
 
-    news = 'Corona Virus'
-    return render_template('index.html',news = news)
+    # news = 'Corona Virus'
+    return render_template('news.html',id = news_id)
